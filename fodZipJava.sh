d2a06c78dd3763832e8c7c312be19bf814023f73
zalmellc <43816554+zalmellc@users.noreply.github.com>
2019-02-27 11:49:32 -0500

    add

--- fodZipJava.sh
+++ fodZipJava.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# Change below to point to you FodUpload.jar path if not in current directory.
-FOD_UPLOAD_JAR=FodUpload.jar
+#
+# Simple Java ZIP Files and Jars for upload to FOD.
+#
 
 if [ "$#" -gt 1 ]; 
 then
@@ -9,17 +10,19 @@ then
 APP_SOURCE_DIR=$1
 APP_JAR_DIR=$2
 
-for java_src in `find ${APP_SOURCE_DIR} -name "*.java" `
-do
-   zip -ur fod_java.zip ${java_src}
+mkdir tmp_fodupload
 
+for appfiles in `find ${APP_SOURCE_DIR} -type f -exec file {} \; | grep "ASCII text" | cut -d: -f1`
+do
+  cp $appfiles tmp_fodupload
 done
 
 for jar_files in `find ${APP_JAR_DIR} -name "*.jar" `
 do
-   zip -ur fod_java.zip ${jar_files}
-
+  cp ${jar_files} tmp_fodupload
 done
+
+zip -r tmp_fodupload.zip tmp_fodupload
 
 else
 
